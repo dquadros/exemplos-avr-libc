fd15c500edf4eeccb654663bd8468841f362a90b
Eduardo Dueñas <eduardo.aug.rib.duenas@gmail.com>
2021-03-29 22:08:20 -0300

    Update main.c

--- exemplos/buzzer/main.c
+++ exemplos/buzzer/main.c
@@ -20,11 +20,14 @@
 #define bitClear(endereco,bit) (endereco &= ~(1<<bit))
 #define bitTest(endereco,bit) (endereco & (1<<bit))
 
-#define NumNotas 10
+#define NumNotas 31
 enum notas{Pausa,Do, DoS, Re, ReS, Mi, Fa, FaS, Sol, SolS, La, LaS, Si, DoM, DoSM, ReM};
-long f[15] = {0xFF,16500000/261.625519,16500000/277.182648,16500000/293.664734,16500000/311.126984,16500000/329.627533,
+long f[15] = long [15]{(long)255L, (long)63067L, (long)59527L, (long)56186L, (long)53033L, 
+            (long)50056L, (long)47247L, (long)44595L, (long)42092L, (long)39729L, (long)37500L, 
+            (long)35395L, (long)33408L, (long)63067L, (long)59527L};
+            /*{0xFF,16500000/261.625519,16500000/277.182648,16500000/293.664734,16500000/311.126984,16500000/329.627533,
             16500000/349.228241,16500000/369.994385,16500000/391.995392,16500000/415.304688,16500000/440,
-            16500000/466.163788,16500000/493.883301,16500000/261.625519,16500000/277.182648,16500000/293.664734};   //valor a ser colocado na flag do timer para cada nota
+            16500000/466.163788,16500000/493.883301,16500000/261.625519,16500000/277.182648,16500000/293.664734};*/   //valor a ser colocado na flag do timer para cada nota
 char Partitura[NumNotas] = {Re,Mi,Mi,Re,Sol,FaS,FaS,FaS,Re,Mi,Mi,Re,La,Sol,Sol,Sol,Re,ReM,ReM,Si,Sol,FaS,FaS,Mi,
                             DoM,Si,Sol,Fa,Sol,Sol,Sol};                          //partitura da música
 char cont = 0;                                          //local da partitura
@@ -50,7 +53,7 @@ void main(){
     //loop infinito
     for(;;){                    
         long i;
-        for(i=0;i>1000000;i++){}         //espera um tempo )altere o valor central para acelerar ou desacelerar a música)
+        for(i=0;i>1000000L;i++){}       //espera um tempo (altere o valor central para acelerar ou desacelerar a música)
         cont++;                         //avança na partitura
         if (cont >= NumNotas)cont=0;    //toca de novo
     }
